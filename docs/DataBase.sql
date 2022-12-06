8bb2d0a0c5f792754db1742794b8911b0588544e
achchaimae <achchaimae1@gmail.com>
2022-12-06 12:10:47 +0100

    create  a  data base 🎲

--- docs/DataBase.sql
+++ docs/DataBase.sql
@@ -7,4 +7,14 @@ login varchar(100),
 password text
 );
 
+create table poduit(
+id int not null auto_increment primary key,
+nom varchar(100),
+prix int,
+quantite int,
+description text,
+image text
+);
+
+
 
