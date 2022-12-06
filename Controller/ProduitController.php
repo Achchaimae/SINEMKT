76b5e54fdd4628676817bcfb140206b46045ae7c
achchaimae <achchaimae1@gmail.com>
2022-12-06 15:33:23 +0100

    controller 🎯

---
+++ Controller/ProduitController.php
@@ -0,0 +1,28 @@
+<?php
+include_once PROJ_DIR . "/models/ProduitModel.php";
+
+class ProduitController{
+    protected $produit;
+    public function __construct(){
+        $this->produit = new Produit();
+    }
+    public function AjouterProduit($nom,$prix,$quantite,$description,$image){
+        $this->produit->AjouterProduit($nom,$prix,$quantite,$description,$image);
+    }
+    public function listproduit(){
+        return $this->produit->listproduit();
+    }
+    public function getproduit($id){
+        return $this->produit->getproduit($id);
+    }
+    public function updateproduit($id,$nom,$prix,$quantite,$description,$image){
+        $this->produit->updateproduit($id,$nom,$prix,$quantite,$description,$image);
+    }
+    public function deleteproduit($id){
+        $this->produit->deleteproduit($id);
+    }
+    public function searchproduit($nom){
+        return $this->produit->searchproduit($nom);
+    }
+
+}
