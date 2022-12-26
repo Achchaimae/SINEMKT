3a4b2d1ea3ced97e1f7455a1c24d45208b9a42ab
achchaimae <achchaimae1@gmail.com>
2022-12-26 11:16:13 +0100

    update style 🐾

--- View/update.php
+++ View/update.php
@@ -1,7 +1,7 @@
 <?php 
  include ('../Controller/productController.php');
     $produit = new prodController();
-    $listproduit = $produit->listproduit();
+    $listproduit = $produit->getproduit($_GET['id']);
     
  ?>
 <!DOCTYPE html>
@@ -10,23 +10,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/mystyle.css">
     <title>Update</title>
 </head>
 <body>      
    
     
-<?php  foreach($listproduit as $key=>$value){ ?>
-            <form action="../index.php" method="POST">
-            <input type="hidden" name="id" value="<?php echo $value['id'] ?>">
-            <input type="text" name="nom" placeholder="nom" value="<?php echo $value['nom'] ?>">
-            <input type="number" name="prix" placeholder="prix" value="<?php echo $value['prix'] ?>">
-            <input type="number" name="quantite" placeholder="quantite" value="<?php echo $value['quantite'] ?>">
-            <textarea name="description"  cols="30" rows="10" placeholder="description"><?php echo $value['description'] ?></textarea>
-            <input type="file" name="image" placeholder="image" src="<?php echo $value['image'] ?>">
+<section class="updateform">
+       
+            <form action="../index.php" method="POST" class="updatefile">
+            <input type="hidden" name="id" value="<?php echo $listproduit['id'] ?>">
+            <label for="nom">nom</label>
+            <input type="text" name="nom" placeholder="nom" value="<?= $listproduit['nom'] ?>">
+            <label for="prix">prix</label>
+            <input type="number" name="prix" placeholder="prix" value="<?php echo $listproduit['prix'] ?>">
+            <label for="quantite">quantite</label>
+            <input type="number" name="quantite" placeholder="quantite" value="<?php echo $listproduit['quantite'] ?>">
+            <label for="description">description</label>
+            <textarea name="description"  cols="30" rows="10" placeholder="description"><?php echo $listproduit['description'] ?></textarea>
+            <label for="image">image</label>
+            <input type="file" name="image" placeholder="image" src="<?php echo $listproduit['image'] ?>">
             <input type="submit" name="update" value="update">
-        </form>
         
-<?php  }
-?>
+        </form>
+
+</section>
 </body>
 </html>
